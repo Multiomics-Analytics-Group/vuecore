ae1ecdf3421646fe73db5a7b8a8b407ada85dc0b
sayalaruano <sebasar1245@gmail.com>
2025-05-20 17:02:30 +0200

    📝 Remove sphinx-copy-button extension

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,6 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_new_tab_link",
     "myst_nb",
-    "sphinx_copybutton",
 ]
 
 #  https://myst-nb.readthedocs.io/en/latest/computation/execute.html
