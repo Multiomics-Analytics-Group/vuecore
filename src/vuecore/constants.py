91b3ae1004b6eaf9a2dbd3ca640129297e56b25f
sayalaruano <sebasar1245@gmail.com>
2025-09-15 16:22:10 +0200

    ✨ Feat(vuecore/engines/plotly/__init__.py): Register violin plot builder and add it to the PlotType StrEnum

--- src/vuecore/constants.py
+++ src/vuecore/constants.py
@@ -14,6 +14,7 @@ class PlotType(StrEnum):
     LINE = auto()
     BAR = auto()
     BOX = auto()
+    VIOLIN = auto()
 
 
 class EngineType(StrEnum):
--- src/vuecore/engines/plotly/__init__.py
+++ src/vuecore/engines/plotly/__init__.py
@@ -5,6 +5,7 @@ from .scatter import build as build_scatter
 from .line import build as build_line
 from .bar import build as build_bar
 from .box import build as build_box
+from .violin import build as build_violin
 from .saver import save
 
 # Import build_utils to ensure it's available
@@ -17,5 +18,6 @@ register_builder(
 register_builder(plot_type=PlotType.LINE, engine=EngineType.PLOTLY, func=build_line)
 register_builder(plot_type=PlotType.BAR, engine=EngineType.PLOTLY, func=build_bar)
 register_builder(plot_type=PlotType.BOX, engine=EngineType.PLOTLY, func=build_box)
+register_builder(plot_type=PlotType.VIOLIN, engine=EngineType.PLOTLY, func=build_violin)
 
 register_saver(engine=EngineType.PLOTLY, func=save)
